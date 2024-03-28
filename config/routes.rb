d5749ef9ceab68a208688608eea754a6f32d8e28
MarcioACOJ <marcio.alexandre.correa.oliveira@gmail.com>
2024-03-28 08:39:31 -0300

    final commit add feature export CSV, use concerns first time

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :tasks, except: %i[show]
-
-  root 'tasks#index'
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root 'tasks#index' 
+  
+  resources :tasks, except: %i[show] do
+    collection do 
+      get :export_csv
+    end
+  end
 end
